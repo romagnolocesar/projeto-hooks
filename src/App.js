f6435975222cf7d14a89bac0f0cf8ae6033d2cbc
Cesar Romagnolo <cesar.romagnolo@hotmail.com>
2021-03-12 13:19:05 -0300

    Aula 81 - UseState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';  
 
 function App() {
+
+  const [tarefas, setTarefas] = useState(['Pagar a conta de luz.', 'Estudar React Hooks']);
+  const [input, setInput] = useState('');
+
+  function handleAdd(){
+    setTarefas([...tarefas, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-        
+        <ul>
+          {tarefas.map(tarefa => (
+              <li key={tarefa} >{tarefa}</li>
+           ) )}
+        </ul>
+        <input type="text" value={input} onChange={e => setInput(e.target.value)}/><br/>
+          <button type="button" onClick={handleAdd}>Adicionar</button>
     </div>
   );
 }
