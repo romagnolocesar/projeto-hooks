a0b6b7b582410a49736caa459ea08785bf8fecd7
Cesar Romagnolo <cesar.romagnolo@hotmail.com>
2021-03-12 13:29:13 -0300

    Aula 82 - UseEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React, {useState} from 'react';  
+import React, {useState, useEffect} from 'react';  
 
 function App() {
 
-  const [tarefas, setTarefas] = useState(['Pagar a conta de luz.', 'Estudar React Hooks']);
+  const [tarefas, setTarefas] = useState([]);
   const [input, setInput] = useState('');
+
+  useEffect( () => {
+    const tarefasStorage = localStorage.getItem('tarefas');
+
+    if(tarefasStorage){
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+  }, []);
+
+
+  useEffect(() => {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }, [tarefas]);
+
 
   function handleAdd(){
     setTarefas([...tarefas, input]);
